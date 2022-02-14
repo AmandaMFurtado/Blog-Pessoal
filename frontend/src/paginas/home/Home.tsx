d1ca9e2ed4f34430b72765f492cf1ef4f30c4f33
Amanda Marques <andaemc@gmail.com>
2022-02-14 14:17:57 -0300

    Create project frontend

---
+++ frontend/src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Home.css';
+
+function Home(){
+    return(
+        <>
+        <h1 className="titulo">Home</h1>
+        <img className="img" src="https://i.imgur.com/UF3OS8o.jpeg" alt="Imagem tela inicial"  />
+        </>
+
+
+    );
+}
+export default Home;
